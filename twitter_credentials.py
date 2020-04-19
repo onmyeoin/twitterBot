2eeb968ba4979946d1fe898b1b80b888fb62fdb7
Eoin <eoinoreilly2@gmail.com>
2020-04-19 11:53:33 +0100

    adding file

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# Sign up for twitter developer account to get access to API credentials
+
+ACCESS_TOKEN = 'Your Token Here'
+ACCESS_TOKEN_SECRET = 'Your Token Secret Here'
+CONSUMER_KEY = 'Your Key Here'
+CONSUMER_SECRET = 'Your Key Secret Here'
